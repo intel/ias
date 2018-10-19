d7664633e813a64a168daa8e749a4bfd2625d4af
Alexandros Frantzis <alexandros.frantzis@collabora.com>
2018-10-19 12:14:11 +0300

    libweston: Introduce zwp_linux_explicit_synchronization_v1
    Introduce support for the zwp_linux_explicit_synchronization_unstable_v1
    protocol with an implementation of the zwp_linux_explicit_synchronization_v1
    interface.
    
    Explicit synchronization provides a more versatile notification
    mechanism for buffer readiness and availability, and can be used to
    improve efficiency by integrating with related functionality in display
    and graphics APIs.
    
    In addition, the per-commit nature of the release events provided by
    this protocol potentially offers a solution to a deficiency of the
    wl_buffer.release event (see
    https://gitlab.freedesktop.org/wayland/wayland/issues/46).
    
    Support for this protocol depends on the capabilities of the backend, so
    we don't register it by default but provide a function which each
    backend will need to call. In this commit only the headless backend when
    using the noop renderer supports this to enable testing.
    
    Note that the zwp_surface_synchronization_v1 interface, which contains
    the core functionality of the protocol, is not implemented in this
    commit. Support for it will be added in future commits.
    
    Changes in v7:
      - Added some information in the commit message about the benefits of
        the explicit sync protocol.
    
    Changes in v6:
      - Fall back to advertising minor version 1 of the explicit sync protocol,
        although we support minor version 2 features, until the new
        wayland-protocols version is released.
    
    Changes in v5:
      - Meson support.
      - Advertise minor version 2 of the explicit sync protocol.
    
    Changes in v4:
      - Enable explicit sync support in the headless backend for all
        renderers.
    
    Changes in v3:
      - Use wl_resource_get_version() instead of hardcoding version 1.
      - Use updated protocol interface names.
      - Use correct format specifier for resource id.
      - Change test name to 'linux-explicit-synchronization.weston'
        (s/_/-/g).
    
    Changes in v2:
      - Move implementation to separate file so protocol can be registered
        on demand by backends.
      - Register protocol in headless+noop backend for testing purposes.
    
    Signed-off-by: Alexandros Frantzis <alexandros.frantzis@collabora.com>

--- libweston/compositor.c
+++ libweston/compositor.c
@@ -2043,6 +2043,11 @@ destroy_surface(struct wl_resource *resource)
 	if (surface->viewport_resource)
 		wl_resource_set_user_data(surface->viewport_resource, NULL);
 
+	if (surface->synchronization_resource) {
+		wl_resource_set_user_data(surface->synchronization_resource,
+					  NULL);
+	}
+
 	weston_surface_destroy(surface);
 }
 
