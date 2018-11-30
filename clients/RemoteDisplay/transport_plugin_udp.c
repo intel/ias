66e0c909440cebcf96a1ba3b213e88a7a2a46368
Satyeshwar Singh <satyeshwar.singh@intel.com>
2018-11-30 15:43:03 -0800

    RemoteDisplay: Added native transport and made it an option
    Now, we can provide remote-display with an option --tp=native/gst.
    This will select the transport mechanism which can either be
    gstreamer based or done natively by the UDP transport plugin.
    
    Note that this is an optional parameter. If this option is not
    provided, then we default to native transport.
    
    Signed-off-by: Satyeshwar Singh <satyeshwar.singh@intel.com>

--- clients/RemoteDisplay/transport_plugin_udp.c
+++ clients/RemoteDisplay/transport_plugin_udp.c
@@ -21,23 +21,34 @@
 #include <stdio.h>
 #include <wayland-util.h>
 #include <libdrm/intel_bufmgr.h>
-
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <arpa/inet.h>
 #include <netdb.h>
-#include "../shared/config-parser.h"
-#include "../shared/helpers.h"
 #include <signal.h>
-
 #include <sys/time.h>
-
-#include "transport_plugin.h"
 #include <gst/gst.h>
 #include <gst/app/gstappsrc.h>
 
+#include "../shared/config-parser.h"
+#include "../shared/helpers.h"
+#include "transport_plugin.h"
+
+
 #define TO_Mb(bytes) ((bytes)/1024/1024*8)
 #define BENCHMARK_INTERVAL 1
+#define RTP_BUFFER_SIZE 1400
+#define RTP_HEADER_SIZE (3*4)
+#define RTP_PAYLOAD_SIZE (RTP_BUFFER_SIZE - RTP_HEADER_SIZE)
+#define NAL_HEADER_SIZE 1
+#define FU_HEADER_SIZE 1
+#define FU_INDICATOR_SIZE 1
+#define NAL_MARKER_SIZE 3
+#define SPS_PPS_MARKER_SIZE 4
+#define NRI_MASK 0x60
+#define NAL_TYPE_MASK 0x1F
+#define FU_A_TYPE 28
 
 struct udpSocket {
 	int sockDesc; struct sockaddr_in sockAddr;
@@ -49,6 +60,7 @@ struct private_data {
 	int debug_packetisation;
 	struct udpSocket socket;
 	char *ipaddr;
+	char *tp;
 	unsigned short port;
 	GstElement *pipeline;
 	GstElement *appsrc;
@@ -77,6 +89,7 @@ WL_EXPORT int init(int *argc, char **argv, void **plugin_private_data, int verbo
 	const struct weston_option options[] = {
 		{ WESTON_OPTION_STRING,  "ipaddr", 0, &private_data->ipaddr},
 		{ WESTON_OPTION_INTEGER, "port", 0, &port},
+		{ WESTON_OPTION_STRING,  "tp", 0, &private_data->tp},
 	};
 	parse_options(options, ARRAY_LENGTH(options), argc, argv);
 	private_data->port = port;
@@ -90,26 +103,49 @@ WL_EXPORT int init(int *argc, char **argv, void **plugin_private_data, int verbo
 		return -1;
 	}
 
-	(void) gst_init (NULL, NULL);
+	if(!private_data->tp) {
+		private_data->tp = strdup("native");
+	}
 
-	pipeline   = gst_pipeline_new ("pipeline");
-	appsrc     = gst_element_factory_make ("appsrc", NULL);
-	h264parse  = gst_element_factory_make ("h264parse", NULL);
-	rtph264pay = gst_element_factory_make ("rtph264pay", NULL);
-	udpsink    = gst_element_factory_make ("udpsink", NULL);
-	if (!pipeline || !appsrc || !h264parse || !rtph264pay || !udpsink)
-		goto gst_free;
+	if(!strcmp(private_data->tp, "gst")) {
+		(void) gst_init (NULL, NULL);
 
-	(void) g_object_set (G_OBJECT (udpsink), "host", private_data->ipaddr, "port", port, NULL);
-	(void) gst_bin_add_many(GST_BIN (pipeline), appsrc, h264parse, rtph264pay, udpsink, NULL);
-	if (!gst_element_link_many (appsrc, h264parse, rtph264pay, udpsink, NULL))
-		goto gst_free_pipeline;
+		pipeline   = gst_pipeline_new ("pipeline");
+		appsrc     = gst_element_factory_make ("appsrc", NULL);
+		h264parse  = gst_element_factory_make ("h264parse", NULL);
+		rtph264pay = gst_element_factory_make ("rtph264pay", NULL);
+		udpsink    = gst_element_factory_make ("udpsink", NULL);
+		if (!pipeline || !appsrc || !h264parse || !rtph264pay || !udpsink)
+			goto gst_free;
 
-	if (gst_element_set_state (pipeline, GST_STATE_PLAYING) == GST_STATE_CHANGE_FAILURE)
-		goto gst_free_pipeline;
+		(void) g_object_set (G_OBJECT (udpsink), "host", private_data->ipaddr, "port", port, NULL);
+		(void) gst_bin_add_many(GST_BIN (pipeline), appsrc, h264parse, rtph264pay, udpsink, NULL);
+		if (!gst_element_link_many (appsrc, h264parse, rtph264pay, udpsink, NULL))
+			goto gst_free_pipeline;
 
-	private_data->appsrc   = appsrc;
-	private_data->pipeline = pipeline;
+		if (gst_element_set_state (pipeline, GST_STATE_PLAYING) == GST_STATE_CHANGE_FAILURE)
+			goto gst_free_pipeline;
+
+		private_data->appsrc   = appsrc;
+		private_data->pipeline = pipeline;
+		printf("Using gstreamer based transport\n");
+
+	} else if(!strcmp(private_data->tp, "native")) {
+
+		private_data->socket.sockDesc = socket(AF_INET, SOCK_DGRAM, 0);
+		if (private_data->socket.sockDesc < 0) {
+			fprintf(stderr, "Socket creation failed.\n");
+			free(private_data);
+			*plugin_private_data = NULL;
+			return -1;
+		}
+
+		private_data->socket.sockAddr.sin_addr.s_addr = inet_addr(private_data->ipaddr);
+		private_data->socket.sockAddr.sin_family = AF_INET;
+		private_data->socket.sockAddr.sin_port = htons(private_data->port);
+
+		printf("Using native transport\n");
+	}
 
 	return 0;
 
@@ -140,12 +176,82 @@ WL_EXPORT void help(void)
 	printf("\tThe udp plugin uses the following parameters:\n");
 	printf("\t--ipaddr=<ip_address>\t\tIP address of receiver.\n");
 	printf("\t--port=<port_number>\t\tPort to use on receiver.\n");
+	printf("\t--tp=<gst/native> (Optional)\t\tTransport mechanism to use."
+			" Either native (default) or gstreamer based\n");
 	printf("\n\tThe receiver should be started using:\n");
 	printf("\t\"gst-launch-1.0 udpsrc port=<port_number>"
 			"! h264parse ! mfxdecode live-mode=true ! mfxsinkelement\"\n");
 }
 
-WL_EXPORT int send_frame(void *plugin_private_data, drm_intel_bo *drm_bo,
+/* This function assumes that we are allowed to overwrite the twelve bytes
+ * that precede the incoming payload address. */
+static int
+send_packet(uint8_t *payload, size_t size, uint32_t timestamp,
+		int marker_bit, struct private_data *private_data)
+{
+	uint8_t *rtpBase8 = (uint8_t *)(payload - RTP_HEADER_SIZE);
+	uint16_t *rtpBase16 = (uint16_t *)(payload - RTP_HEADER_SIZE);
+	uint32_t *rtpBase32 = (uint32_t *)(payload - RTP_HEADER_SIZE);
+	static uint16_t sequence_number = 1;
+
+	if (size > RTP_PAYLOAD_SIZE) {
+		fprintf(stderr, "Payload size %d too large (>1388).\n", (int)size);
+		return 1;
+	}
+
+	/* Fill packet using h.264 RTP style */
+	rtpBase8[0] = 0x80; /* RFC 1889 version(2) */
+	if (marker_bit) {
+		rtpBase8[1] = 0xE0; /* marker bit + payload type: hardcoded H264 */
+	} else {
+		rtpBase8[1] = 0x60; /* no marker bit + payload type: hardcoded H264 */
+	}
+
+	rtpBase32[1] = htonl(timestamp);
+	rtpBase32[2] = htonl(0x4120db95); /* SSRC hard-coded */
+	rtpBase16[1] = htons(sequence_number++);
+
+	if (!private_data) {
+		fprintf(stderr, "No private data!\n");
+		return -1;
+	}
+
+	int rval = sendto(private_data->socket.sockDesc, rtpBase8, size + RTP_HEADER_SIZE, 0,
+			(struct sockaddr *) &private_data->socket.sockAddr, sizeof(private_data->socket.sockAddr));
+
+	if (rval <= 0) {
+		fprintf(stderr, "Send failed with %m\n");
+	}
+
+	return 0;
+}
+
+static int
+get_ps_write_size(uint8_t *readptr)
+{
+	int i = 0;
+	int found_end = 0;
+
+	while (!found_end) {
+		if (readptr[i] == 0x00) {
+			if (readptr[i+1] == 0x00) {
+				if (readptr[i+2] == 0x00) {
+					if (readptr[i+3] == 0x01) {
+						found_end = 1;
+						continue;
+					}
+				} else if (readptr[i+2] == 0x01) {
+					found_end = 1;
+					continue;
+				}
+			}
+		}
+		i++;
+	}
+	return i;
+}
+
+static int send_frame_gst(void *plugin_private_data, drm_intel_bo *drm_bo,
 		int32_t stream_size, uint32_t timestamp)
 {
 	uint8_t *readptr = drm_bo->virtual;
@@ -205,6 +311,270 @@ error:
 	return -1;
 }
 
+static int send_frame_native(void *plugin_private_data, drm_intel_bo *drm_bo,
+		int32_t stream_size, uint32_t timestamp)
+{
+	int num_packets = 0;
+	int bytes_written = 0;
+	uint16_t count = 0;
+	uint8_t rtp_buffer[RTP_BUFFER_SIZE];
+	uint8_t *rtp_payload = &rtp_buffer[RTP_HEADER_SIZE];
+	uint8_t *fu_start;
+	uint8_t *readptr = drm_bo->virtual;
+	int32_t write_size = stream_size;
+	uint8_t nal_header = 0;
+	int start = 1, end = 0;
+	/* Allow for FU indicator size and FU header size */
+	const int step = RTP_PAYLOAD_SIZE - FU_HEADER_SIZE - FU_INDICATOR_SIZE;
+	int spspps = 0;
+	uint8_t *bufdata;
+	volatile sig_atomic_t send_packages;
+	int err = 0;
+
+	struct private_data *private_data = (struct private_data *)plugin_private_data;
+
+	if (!private_data) {
+		fprintf(stderr, "No private data!\n");
+		return -1;
+	}
+
+	if (private_data->verbose) {
+		printf("Sending frame over AVB...\n");
+	}
+
+	/* SPS and PPS are preceded by the bytes 00 00 00 01 (in our case).
+	 * 00 00 01 means we have an ordinary NAL unit containing an h264
+	 * encoded frame. This may well be different and more nuanced with
+	 * other implementations. */
+	bufdata = ((uint8_t *)(drm_bo->virtual));
+	if (bufdata[0] == 0x00) {
+		if (bufdata[1] == 0x00) {
+			if (bufdata[2] == 0x00) {
+				if (bufdata[3] == 0x01) {
+					/* SPS or PPS */
+					if (private_data->debug_packetisation) {
+						printf("SPS or PPS frame\n");
+					}
+					spspps = 1;
+				} else {
+					fprintf(stderr, "Invalid start of stream.\n");
+					return 1;
+				}
+			} else if (bufdata[2] == 0x01) {
+				/* Ordinary NAL unit containing an h264 encoded frame */
+				if (private_data->debug_packetisation) {
+					printf("00 00 01 - start of frame?\n");
+				}
+			} else {
+				fprintf(stderr, "Invalid frame.\n");
+				return 1;
+			}
+		}
+	}
+
+	send_packages = 1;
+	while (send_packages){
+		num_packets++;
+		if (spspps == 1) {
+			int32_t sps_size, pps_size;
+			/* Skip 00 00 00 01 */
+			readptr += SPS_PPS_MARKER_SIZE;
+			sps_size = get_ps_write_size(readptr);
+
+			/* Send SPS... */
+			memcpy(rtp_payload, readptr, sps_size);
+			err = send_packet(rtp_payload, sps_size, timestamp, 1,
+					private_data);
+			bytes_written += sps_size;
+			if (err) {
+				fprintf(stderr, "Warning: Sending SPS packet returned %d.\n",
+						err);
+				err = 0;
+			}
+
+			/* Advance readptr by SPS + second 00 00 00 01 marker... */
+			readptr += sps_size + SPS_PPS_MARKER_SIZE;
+			pps_size = get_ps_write_size(readptr);
+
+			if (private_data->debug_packetisation) {
+				printf("Skipping second 00 00 00 01 marker and writing"
+				   " %d bytes + 12 byte header\n", pps_size);
+				nal_header = readptr[0];
+				printf("PPS - nal_type = 0x%x\n",
+					nal_header & NAL_TYPE_MASK);
+			}
+
+			/* Send PPS... */
+			/* We avoid doing a memcpy where possible, writing
+			 * the headers to the mapped buffer instead. (This
+			 * trashes the previous 12 bytes in the buffer, but we
+			 * don't need to worry about that.) */
+			if (bytes_written >= RTP_HEADER_SIZE) {
+				err = send_packet(readptr, pps_size, timestamp,
+					1, private_data);
+			} else {
+				memcpy(rtp_payload, readptr, pps_size);
+				err = send_packet(rtp_payload, pps_size, timestamp, 1,
+						private_data);
+			}
+			bytes_written += pps_size;
+			if (err) {
+				fprintf(stderr, "Warning: Sending PPS packet returned %d.\n",
+						err);
+				err = 0;
+			}
+
+			/* There will be a NAL unit in buffer after SPS and PPS. */
+			readptr += pps_size;
+			write_size = stream_size -
+				(sps_size + pps_size + 2 * SPS_PPS_MARKER_SIZE);
+			spspps = 0;
+		}
+
+		if (start == 1) {
+			/* Skip 00 00 01 marker before NAL unit starts... */
+			readptr += NAL_MARKER_SIZE;
+			write_size -= NAL_MARKER_SIZE;
+			nal_header = readptr[0];
+			if (private_data->debug_packetisation) {
+				printf("Skipped 00 00 01 marker.\n");
+				printf("nal_type = 0x%x\n",
+					nal_header & NAL_TYPE_MASK);
+			}
+		}
+
+		/* NAL packetisation into Fragmentation Units (FUs)...*/
+		if (RTP_PAYLOAD_SIZE >= write_size) {
+			/* Stream is smaller than a packet, no FUs. */
+			if (private_data->debug_packetisation) {
+				printf("Small packet, only writing %d bytes.\n", write_size);
+			}
+			if (bytes_written >= RTP_HEADER_SIZE) {
+				err = send_packet(readptr, write_size, timestamp,
+					1, private_data);
+			} else {
+				memcpy(rtp_payload, readptr, write_size);
+				err = send_packet(rtp_payload, write_size, timestamp, 1,
+						private_data);
+			}
+			bytes_written += write_size;
+			if (err) {
+				fprintf(stderr, "Warning: Sending small packet returned %d.\n",
+						err);
+				err = 0;
+			}
+			send_packages = 0;
+		} else if (step * count < write_size) {
+			count++;
+
+			/* FU indicator - as per section 5.8 of rfc6184. */
+			*rtp_payload = (nal_header & NRI_MASK) | FU_A_TYPE;
+
+			/* FU header - as per section 5.8 of rfc6184. */
+			*(rtp_payload + 1) = (start << 7) | (end << 6) |
+				(nal_header & NAL_TYPE_MASK);
+
+			if (private_data->debug_packetisation) {
+				printf("%s FU. Indicator 0x%x Header 0x%x\n",
+					start ? "First" : "Middle",
+					/* Sometimes fprintf tries to interpret
+					 * these as 64-bit ints. The 0xFF & fixes that. */
+					0xFF & *rtp_payload, 0xFF & *(rtp_payload + 1));
+			}
+
+			if (start) {
+				/* Skip the NAL header because the info is
+				 * already in the FU indicator and header. */
+				readptr += NAL_HEADER_SIZE;
+				start = 0;
+			}
+
+			if (bytes_written >= RTP_HEADER_SIZE) {
+				/* We have the marker bits free as well as the header size. */
+				fu_start = readptr - FU_HEADER_SIZE - FU_INDICATOR_SIZE;
+				fu_start[0] = *rtp_payload;
+				fu_start[1] = *(rtp_payload + 1);
+				err = send_packet(fu_start, RTP_PAYLOAD_SIZE,
+					timestamp, 0, private_data);
+			} else {
+				memcpy(rtp_payload + FU_HEADER_SIZE + FU_INDICATOR_SIZE,
+					readptr, step);
+				err = send_packet(rtp_payload, RTP_PAYLOAD_SIZE,
+					timestamp, 0, private_data);
+			}
+			bytes_written += step;
+			if (err) {
+				fprintf(stderr, "Warning: Sending FU packet returned %d.\n",
+						err);
+				err = 0;
+			}
+
+			readptr += step;
+		} else {
+			/* Last FU... */
+			int bytes_left = write_size - (step*(count-1)) - NAL_HEADER_SIZE;
+
+			end = 1;
+			*rtp_payload = (nal_header & NRI_MASK) | FU_A_TYPE;
+			*(rtp_payload + 1) = (start << 7) | (end << 6) |
+						(nal_header & NAL_TYPE_MASK);
+
+			if (private_data->debug_packetisation) {
+				printf("Last FU. Indicator: 0x%x, Header: 0x%x, size: %d\n",
+					*rtp_payload, *(rtp_payload + 1), bytes_left);
+			}
+
+			if (bytes_written >= RTP_HEADER_SIZE) {
+				/* We have the marker bits free as well as the header size. */
+				fu_start = readptr - FU_HEADER_SIZE - FU_INDICATOR_SIZE;
+				fu_start[0] = *rtp_payload;
+				fu_start[1] = *(rtp_payload + 1);
+				err = send_packet(fu_start, bytes_left
+					+ FU_HEADER_SIZE + FU_INDICATOR_SIZE,
+					timestamp, 1, private_data);
+			} else {
+				memcpy(rtp_payload + FU_HEADER_SIZE
+					+ FU_INDICATOR_SIZE,
+					readptr, bytes_left);
+				err = send_packet(rtp_payload, bytes_left
+					+ FU_HEADER_SIZE + FU_INDICATOR_SIZE,
+					timestamp, 1, private_data);
+			}
+			bytes_written += bytes_left;
+			if (err) {
+				fprintf(stderr, "Warning: Sending final packet returned %d.\n",
+						err);
+				err = 0;
+			}
+
+			send_packages = 0;
+		}
+	}
+
+	if (private_data->verbose || private_data->debug_packetisation) {
+		printf("Packets for frame = %d packets.\n", num_packets);
+	}
+	return 0;
+}
+
+WL_EXPORT int send_frame(void *plugin_private_data, drm_intel_bo *drm_bo,
+		int32_t stream_size, uint32_t timestamp)
+{
+	struct private_data *private_data = (struct private_data *)plugin_private_data;
+
+	if(!private_data) {
+		return -1;
+	} else {
+		return !strcmp(private_data->tp, "gst")
+				? send_frame_gst(plugin_private_data, drm_bo,
+						stream_size, timestamp)
+				: send_frame_native(plugin_private_data, drm_bo,
+						stream_size, timestamp);
+	}
+}
+
+
+
 WL_EXPORT void destroy(void **plugin_private_data)
 {
 	struct private_data *private_data = (struct private_data *)*plugin_private_data;
@@ -217,14 +587,21 @@ WL_EXPORT void destroy(void **plugin_private_data)
 		fprintf(stdout, "Closing network connection...\n");
 	}
 
-	if (private_data->pipeline) {
+	if(!strcmp(private_data->tp, "gst") && private_data->pipeline) {
 		(void) gst_element_set_state (private_data->pipeline, GST_STATE_NULL);
 		(void) gst_object_unref (GST_OBJECT (private_data->pipeline));
+	} else if(!strcmp(private_data->tp, "native")) {
+		close(private_data->socket.sockDesc);
+		private_data->socket.sockDesc = -1;
+		memset(&private_data->socket.sockAddr, 0, sizeof(private_data->socket.sockAddr));
 	}
 
 	if (private_data->verbose) {
 		fprintf(stdout, "Freeing plugin private data...\n");
 	}
+
+	free(private_data->ipaddr);
+	free(private_data->tp);
 	free(private_data);
 	*plugin_private_data = NULL;
 }
