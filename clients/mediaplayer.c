5a0245180787dac58ea6690d2b9aef6ee9d144ae
Chae, Alex <alex.chae@intel.com>
2019-02-17 12:22:03 +0900

    mediaplayer: Support audio and textoverlay as an option
    Changed option input method by using cmdline option
    parser.
    Enabled media audio track support and added an arg to
    options (-a <0:w/o audio, 1:w/ audio>).
    Enabled textoverlay to print "[hostname] - [filename]"
    over video stream to distinguish where the video plays
    and added an arg to options (-t <0:off, 1:always, 2:blink>).
    
    Signed-off-by: Chae, Alex <alex.chae@intel.com>
    Reviewed-by: Singh, Satyeshwar <satyeshwar.singh@intel.com>

--- clients/mediaplayer.c
+++ clients/mediaplayer.c
@@ -24,225 +24,378 @@
  */
 
 #include <stdio.h>
+#include <unistd.h>
+#include <libgen.h>
 #include <gst/gst.h>
 #include <glib.h>
+#include <glib/gi18n.h>
 #include <gst/video/videooverlay.h>
 #include <gst/video/gstvideosink.h>
 
-#define XCASESTRING(A) case A: return #A;
-
-char *GetT(unsigned int a)
-{
-	switch(a) {
-		XCASESTRING(GST_STREAM_STATUS_TYPE_CREATE );
-		XCASESTRING(GST_STREAM_STATUS_TYPE_ENTER  );
-		XCASESTRING(GST_STREAM_STATUS_TYPE_LEAVE  );
-		XCASESTRING(GST_STREAM_STATUS_TYPE_DESTROY);
-		XCASESTRING(GST_STREAM_STATUS_TYPE_START  );
-		XCASESTRING(GST_STREAM_STATUS_TYPE_PAUSE  );
-		XCASESTRING(GST_STREAM_STATUS_TYPE_STOP   );
-	}
-	return "???";
-}
-
-char *GetM(unsigned int a)
-{
-	switch (a) {
-		XCASESTRING(  GST_MESSAGE_EOS               );
-		XCASESTRING(  GST_MESSAGE_ERROR             );
-		XCASESTRING(  GST_MESSAGE_WARNING           );
-		XCASESTRING(  GST_MESSAGE_INFO              );
-		XCASESTRING(  GST_MESSAGE_TAG               );
-		XCASESTRING(  GST_MESSAGE_BUFFERING         );
-		XCASESTRING(  GST_MESSAGE_STATE_CHANGED     );
-		XCASESTRING(  GST_MESSAGE_STATE_DIRTY       );
-		XCASESTRING(  GST_MESSAGE_STEP_DONE         );
-		XCASESTRING(  GST_MESSAGE_CLOCK_PROVIDE     );
-		XCASESTRING(  GST_MESSAGE_CLOCK_LOST        );
-		XCASESTRING(  GST_MESSAGE_NEW_CLOCK         );
-		XCASESTRING(  GST_MESSAGE_STRUCTURE_CHANGE  );
-		XCASESTRING(  GST_MESSAGE_STREAM_STATUS     );
-		XCASESTRING(  GST_MESSAGE_APPLICATION       );
-		XCASESTRING(  GST_MESSAGE_ELEMENT           );
-		XCASESTRING(  GST_MESSAGE_SEGMENT_START     );
-		XCASESTRING(  GST_MESSAGE_SEGMENT_DONE      );
-		XCASESTRING(  GST_MESSAGE_DURATION_CHANGED  );
-		XCASESTRING(  GST_MESSAGE_LATENCY           );
-		XCASESTRING(  GST_MESSAGE_ASYNC_START       );
-		XCASESTRING(  GST_MESSAGE_ASYNC_DONE        );
-		XCASESTRING(  GST_MESSAGE_REQUEST_STATE     );
-		XCASESTRING(  GST_MESSAGE_STEP_START        );
-		XCASESTRING(  GST_MESSAGE_QOS               );
-		XCASESTRING(  GST_MESSAGE_PROGRESS          );
-		XCASESTRING(  GST_MESSAGE_TOC               );
-		XCASESTRING(  GST_MESSAGE_RESET_TIME        );
-		XCASESTRING(  GST_MESSAGE_STREAM_START      );
-		XCASESTRING(  GST_MESSAGE_NEED_CONTEXT      );
-		XCASESTRING(  GST_MESSAGE_HAVE_CONTEXT      );
-	}
-	return "???";
-}
+#define CLOCK_START_TIME 5 // seconds
+#define CLOCK_INTERVAL 2   // seconds
 
 typedef struct _AppData
 {
-    GstElement *pipeline;
-    GstElement *source;
-    GstElement *demuxer;
-    GstElement *parser;
-    GstElement *decoder;
-    GstElement *sink;
-    GMainLoop *loop;
-    gboolean looping;
+	GstElement *pipeline;
+	GstElement *source;
+	GstElement *demuxer;
+	GstElement *videoqueue;
+	GstElement *parser;
+	GstElement *decoder;
+	GstElement *textoverlay;
+	GstElement *videosink;
+	GstElement *audioqueue;
+	GstElement *decodebin;
+	GstElement *audioconvert;
+	GstElement *audioresample;
+	GstElement *audiosink;
+	GMainLoop *loop;
+	gboolean looping;
+	gboolean audio;
+	guint printtext;
+	gchar text[64];
 } AppData;
 
 static gboolean bus_call (GstBus *bus, GstMessage *msg, gpointer data)
 {
-    AppData *appdata = (AppData *) data;
-    GMainLoop *loop = appdata->loop;
+	AppData *appdata = (AppData *) data;
+	GMainLoop *loop = appdata->loop;
 
-    switch (GST_MESSAGE_TYPE (msg)) {
+	switch (GST_MESSAGE_TYPE (msg)) {
 
-        case GST_MESSAGE_EOS:
-            g_print ("End of stream\n");
-            if (appdata->looping) {
-                /* restart playback if at end */
-                if (!gst_element_seek(appdata->pipeline,
-                            1.0, GST_FORMAT_TIME, GST_SEEK_FLAG_FLUSH,
-                            GST_SEEK_TYPE_SET, 0,
-                            GST_SEEK_TYPE_NONE, GST_CLOCK_TIME_NONE)) {
-                    g_printerr("Seek failed and stop playback!\n");
-                    g_main_loop_quit (loop);
-                }
-            }
-            else
-                    g_main_loop_quit (loop);
-            break;
+		case GST_MESSAGE_EOS:
+			g_print ("End of stream\n");
+			if (appdata->looping) {
+				/* restart playback if at end */
+				if (!gst_element_seek(appdata->pipeline,
+							1.0, GST_FORMAT_TIME, GST_SEEK_FLAG_FLUSH,
+							GST_SEEK_TYPE_SET, 0,
+							GST_SEEK_TYPE_NONE, GST_CLOCK_TIME_NONE)) {
+					g_printerr("Seek failed and stop playback!\n");
+					g_main_loop_quit (loop);
+				}
+			}
+			else
+				g_main_loop_quit (loop);
+			break;
 
-        case GST_MESSAGE_ERROR:
-            {
-                gchar  *debug;
-                GError *error;
+		case GST_MESSAGE_ERROR:
+			{
+				gchar  *debug;
+				GError *error;
 
-                gst_message_parse_error (msg, &error, &debug);
-                g_free (debug);
+				gst_message_parse_error (msg, &error, &debug);
+				g_free (debug);
 
-                g_printerr ("Error: %s\n", error->message);
-                g_error_free (error);
+				g_printerr ("Error: %s\n", error->message);
+				g_error_free (error);
 
-                g_main_loop_quit (loop);
-                break;
-            }
-        default:
-            //g_print("unhandle %s\n", GetM(GST_MESSAGE_TYPE(msg)));
-            break;
-    }
+				g_main_loop_quit (loop);
+				break;
+			}
+		default:
+			break;
+	}
 
-    return TRUE;
+	return TRUE;
 }
 
+static gboolean timeout_reached (GstClock *clock, GstClockTime time, GstClockID id, gpointer data)
+{
+	AppData *appdata = (AppData *) data;
+
+	if (appdata->printtext == 2) {
+		g_object_set (G_OBJECT (appdata->textoverlay), "text", "", NULL);
+		appdata->printtext = 0;
+	}
+	else {
+		g_object_set (G_OBJECT (appdata->textoverlay), "text", appdata->text, NULL);
+		appdata->printtext = 2;
+	}
+
+	return TRUE;
+}
 
 static void on_pad_added (GstElement *element, GstPad *pad, gpointer data)
 {
-    GstPad *sinkpad;
-    GstElement *decoder = (GstElement *) data;
+	GstPad *sinkpad;
+	AppData *appdata = (AppData *) data;
 
-    sinkpad = gst_element_get_static_pad (decoder, "sink");
+	sinkpad = gst_element_get_static_pad (appdata->parser, "sink");
 
-    gst_pad_link (pad, sinkpad);
-
-    gst_object_unref (sinkpad);
+	gst_pad_link (pad, sinkpad);
+	gst_object_unref (sinkpad);
 }
 
-static void usage()
+static void demux_on_pad_added (GstElement *element, GstPad *pad, gpointer data)
 {
-    g_printerr ("\nUsage:\n\t mediaplayer <Video filename> <x,y,w,h> <loop>\n\n");
-    g_printerr ("This is a H264 content player that plays a content repeatedly"
-            "with the same surface id for 2019 CES demo.\n"
-            "\tex) mediaplayer sample.mp4 0,0,1920,1080 loop\n");
-    exit(-1);
+	GstPad *sinkpad = NULL;
+	AppData *appdata = (AppData *) data;
+	gchar *name;
+
+	name = gst_pad_get_name (pad);
+	if (g_strrstr(name, "video_0")) {
+		sinkpad = gst_element_get_static_pad (appdata->videoqueue, "sink");
+		gst_pad_link (pad, sinkpad);
+	} else if (g_strrstr(name, "audio_0")) {
+		sinkpad = gst_element_get_static_pad (appdata->audioqueue, "sink");
+		gst_pad_link (pad, sinkpad);
+	}
+
+	if (sinkpad != NULL)
+		gst_object_unref (sinkpad);
+
+	g_free (name);
+}
+
+static void decode_on_pad_added (GstElement *element, GstPad *pad, gpointer data)
+{
+	AppData *appdata = (AppData *) data;
+	GstCaps *caps;
+	GstStructure *str;
+	GstPad *audiopad;
+
+	/* only link once */
+	audiopad = gst_element_get_static_pad (appdata->audioconvert, "sink");
+	if (GST_PAD_IS_LINKED (audiopad)) {
+		g_object_unref (audiopad);
+		return;
+	}
+
+	/* check media type */
+	caps = gst_pad_query_caps (pad, NULL);
+	str = gst_caps_get_structure (caps, 0);
+	if (!g_strrstr (gst_structure_get_name (str), "audio")) {
+		gst_caps_unref (caps);
+		gst_object_unref (audiopad);
+		return;
+	}
+	gst_caps_unref (caps);
+
+	gst_pad_link (pad, audiopad);
+	g_object_unref (audiopad);
+}
+
+static void usage (void)
+{
+	g_printerr ("\nUsage:\n\t mediaplayer -f <Video filename> -r <x,y,w,h> -l <0:off, 1:loop> -a <0:w/o audio, 1:w/ audio> -t <0:off, 1:always, 2:blink>\n\n");
+	g_printerr ("This is a H264 content player that plays a content repeatedly "
+			"with the same surface id for 2019 CES demo.\n"
+			"ex) mediaplayer -f sample.mp4 -r 0,0,1920,1080 -l 1 -a 1 -t 1\n");
+	exit(-1);
 }
 
 int main (int argc, char *argv[])
 {
-    AppData appdata;
-    GstBus *bus;
-    guint bus_watch_id;
-    guint x = 0, y = 0, w = 3440, h = 1440;
+	AppData appdata = {0,};
+	GstBus *bus;
+	guint bus_watch_id;
+	gchar *rect = NULL;
+	gchar *filename = NULL;
+	guint x = 0, y = 0, w = 3440, h = 1440;
+	GOptionContext *ctx;
+	GError *err = NULL;
+	GstClock *clock = NULL;
+	GstClockID id = 0;
+	GstClockTime start_time = 0;
+	GstClockTime interval = CLOCK_INTERVAL;
+	GOptionEntry options[] = {
+		{"loop", 'l', 0, G_OPTION_ARG_INT, &appdata.looping,
+			N_("Play video indefinitely"), NULL},
+		{"rect", 'r', 0, G_OPTION_ARG_STRING, &rect,
+			N_("Set render rectangle, <x,y,w,h>"), NULL},
+		{"file", 'f', 0, G_OPTION_ARG_STRING, &filename,
+			N_("Media file location"), NULL},
+		{"audio", 'a', 0, G_OPTION_ARG_INT, &appdata.audio,
+			N_("Play audio as well"), NULL},
+		{"text", 't', 0, G_OPTION_ARG_INT, &appdata.printtext,
+			N_("Play audio as well"), NULL},
+		{NULL}
+	};
 
-    /* Initialisation */
-    gst_init (&argc, &argv);
+	/* Initialisation */
+	appdata.looping = 0;
+	appdata.audio = 0;
+	ctx = g_option_context_new ("ARGUMENTS");
+	g_option_context_add_main_entries (ctx, options, NULL);
+	g_option_context_add_group (ctx, gst_init_get_option_group ());
+	if (!g_option_context_parse (ctx, &argc, &argv, &err)) {
+		if (err)
+			g_printerr ("Error initializing: %s\n", GST_STR_NULL (err->message));
+		else
+			g_printerr ("Error initializing: Unknown error!\n");
+		g_clear_error (&err);
+		g_option_context_free (ctx);
+		return -1;
+	}
+	g_option_context_free (ctx);
 
-    appdata.loop = g_main_loop_new (NULL, FALSE);
+	if (filename == NULL)
+		usage();
 
-    /* Create gstreamer elements */
-    appdata.pipeline = gst_pipeline_new ("video-player");
-    appdata.source   = gst_element_factory_make ("filesrc", "file-source");
-    appdata.demuxer  = gst_element_factory_make ("qtdemux", "qt-demuxer");
-    appdata.parser   = gst_element_factory_make ("h264parse", "h264-parser");
-    appdata.decoder  = gst_element_factory_make ("msdkh264dec", "msdk-decoder");
-    appdata.sink     = gst_element_factory_make ("glimagesink", "video-output");
+	if (rect != NULL)
+		sscanf(rect, "%d,%d,%d,%d", &x, &y, &w, &h);
 
-    if (!appdata.pipeline || !appdata.source || !appdata.demuxer || !appdata.parser || !appdata.decoder || !appdata.sink) {
-        g_printerr ("One element could not be created. Exiting.\n");
-        return -1;
-    }
+	appdata.loop = g_main_loop_new (NULL, FALSE);
 
-    /* Check input arguments */
-    if (argc == 4) {
-        if (!strcmp(argv[3], "loop"))
-            appdata.looping = TRUE;
-        else
-            appdata.looping = FALSE;
-        sscanf(argv[2], "%d,%d,%d,%d", &x, &y, &w, &h);
-    }
-    else if (argc == 3) {
-		appdata.looping = FALSE;
-        sscanf(argv[2], "%d,%d,%d,%d", &x, &y, &w, &h);
-    }
-    else if (argc != 2) {
-        usage();
-    }
+	/* Create gstreamer elements */
+	appdata.pipeline = gst_pipeline_new ("video-player");
+	appdata.source   = gst_element_factory_make ("filesrc", "file-source");
+	if (appdata.audio) {
+		g_print ("Playing audio as well\n");
+		appdata.audioqueue   = gst_element_factory_make ("queue", "audio_queue");
+		appdata.decodebin = gst_element_factory_make ("decodebin", "audio_decode");
+		appdata.audioconvert   = gst_element_factory_make ("audioconvert", "audio_convert");
+		appdata.audioresample   = gst_element_factory_make ("audioresample", "audio_resample");
+		appdata.audiosink   = gst_element_factory_make ("autoaudiosink", "audio_sink");
+		appdata.videoqueue   = gst_element_factory_make ("queue", "video_queue");
+	} else {
+		g_print ("Playing video only\n");
+	}
+	appdata.demuxer  = gst_element_factory_make ("qtdemux", "qt-demuxer");
+	appdata.parser   = gst_element_factory_make ("h264parse", "h264-parser");
+	appdata.decoder  = gst_element_factory_make ("msdkh264dec", "msdk-decoder");
+	appdata.textoverlay = gst_element_factory_make ("textoverlay", "text-overlay");
+	appdata.videosink     = gst_element_factory_make ("glimagesink", "video-output");
 
-    /* Set up the pipeline */
+	if (appdata.audio) {
+		if (!appdata.pipeline || !appdata.source || !appdata.demuxer || \
+				!appdata.parser || !appdata.decoder || !appdata.textoverlay || !appdata.videosink || \
+				!appdata.audioqueue || !appdata.decodebin || !appdata.audioconvert || \
+				!appdata.audioresample || !appdata.audiosink) {
+			g_printerr ("One element could not be created. Exiting.\n");
+			return -1;
+		}
+	} else {
+		if (!appdata.pipeline || !appdata.source || !appdata.demuxer || \
+				!appdata.parser || !appdata.decoder || !appdata.textoverlay || !appdata.videosink) {
+			g_printerr ("One element could not be created. Exiting.\n");
+			return -1;
+		}
+	}
 
-    /* Set the input filename to the source element */
-    g_object_set (G_OBJECT (appdata.source), "location", argv[1], NULL);
+	/* Set up the pipeline */
+	/* Set the input filename to the source element */
+	g_object_set (G_OBJECT (appdata.source), "location", filename, NULL);
 
-    /* Set render ractangle for window */
-    gst_video_overlay_set_render_rectangle(GST_VIDEO_OVERLAY (appdata.sink), x, y, w, h);
+	/* set text to textoverlay in format of [hostname] - [filename] */
+	if (appdata.printtext) {
+		gethostname(appdata.text, 10);
+		sprintf(appdata.text+strlen(appdata.text), " - %s", basename(filename));
+		g_object_set (G_OBJECT (appdata.textoverlay), "text", appdata.text, NULL);
+	}
 
-    /* Add a message handler */
-    bus = gst_pipeline_get_bus (GST_PIPELINE (appdata.pipeline));
-    bus_watch_id = gst_bus_add_watch (bus, bus_call, &appdata);
-    gst_object_unref (bus);
+	/* Set render ractangle for window */
+	gst_video_overlay_set_render_rectangle(GST_VIDEO_OVERLAY (appdata.videosink), x, y, w, h);
 
-    /* Add all elements into the pipeline */
-    /* file-source | qt-demuxer | h264-parser | msdk-decoder | video-output */
-    gst_bin_add_many (GST_BIN (appdata.pipeline),
-            appdata.source, appdata.demuxer, appdata.parser, appdata.decoder, appdata.sink, NULL);
+	/* Add a message handler */
+	bus = gst_pipeline_get_bus (GST_PIPELINE (appdata.pipeline));
+	bus_watch_id = gst_bus_add_watch (bus, bus_call, &appdata);
+	gst_object_unref (bus);
 
-    /* Link the elements together */
-    gst_element_link (appdata.source, appdata.demuxer);
-    gst_element_link_many (appdata.parser, appdata.decoder, appdata.sink, NULL);
-    g_signal_connect (appdata.demuxer, "pad-added", G_CALLBACK (on_pad_added), appdata.parser);
+	/* Add all elements into the pipeline */
+	if (appdata.audio) {
+		gst_bin_add_many (GST_BIN (appdata.pipeline), appdata.source, \
+				appdata.demuxer, appdata.videoqueue, appdata.parser, \
+				appdata.decoder, appdata.textoverlay, appdata.videosink, appdata.audioqueue, \
+				appdata.decodebin, appdata.audioconvert, appdata.audioresample, \
+				appdata.audiosink, NULL);
+	} else {
+		gst_bin_add_many (GST_BIN (appdata.pipeline), appdata.source, \
+				appdata.demuxer, appdata.parser, appdata.decoder, \
+				appdata.textoverlay, appdata.videosink, NULL);
+	}
 
-    /* Set the pipeline to "playing" state*/
-    g_print ("Now playing: %s\n", argv[1]);
-    gst_element_set_state (appdata.pipeline, GST_STATE_PLAYING);
+	/* Link the elements */
+	/* Link#1 : source -> demux */
+	if (gst_element_link_many (appdata.source, appdata.demuxer, NULL) != TRUE) {
+		g_printerr(" Error: main linking\n");
+		goto exit;
+	}
 
-    /* Iterate */
-    g_print ("Running...\n");
-    g_main_loop_run (appdata.loop);
+	if (appdata.audio) {
+		/* Link#2 - video : queue -> parse -> decode -> sink */
+		if (gst_element_link_many (appdata.videoqueue, appdata.parser, \
+					appdata.decoder, appdata.textoverlay, appdata.videosink, NULL) != TRUE) {
+			g_printerr("Error: video linking\n");
+			goto exit;
+		}
+
+		/* Link#3 - audio : audioconvert -> audioresample -> audiosink */
+		if ( gst_element_link_many (appdata.audioconvert, \
+					appdata.audioresample, appdata.audiosink, NULL) != TRUE ) {
+			g_printerr(" Error: audio linking\n");
+			goto exit;
+		}
+
+		/* Link#4 : audioqueue ->  decodebin */
+		if ( gst_element_link (appdata.audioqueue, appdata.decodebin) != TRUE ) {
+			g_printerr(" Error: decodebin linking\n");
+			goto exit;
+		}
+	} else {
+		/* Link#2 - video : parse -> decode -> sink */
+		if (gst_element_link_many (appdata.parser, \
+					appdata.decoder, appdata.textoverlay, appdata.videosink, NULL) != TRUE) {
+			g_printerr("Error: video linking\n");
+			goto exit;
+		}
+	}
+
+	if (appdata.audio) {
+		g_signal_connect (appdata.demuxer, "pad-added", \
+				G_CALLBACK (demux_on_pad_added), &appdata);
+		g_signal_connect (appdata.decodebin, "pad-added", \
+				G_CALLBACK (decode_on_pad_added), &appdata);
+	} else {
+		g_signal_connect (appdata.demuxer, "pad-added", \
+				G_CALLBACK (on_pad_added), &appdata);
+	}
+
+	/* Set the pipeline to "playing" state*/
+	g_print ("Now playing: %s\n", filename);
+	gst_element_set_state (appdata.pipeline, GST_STATE_PLAYING);
+
+	if (appdata.printtext == 2) {
+		clock = gst_pipeline_get_pipeline_clock(GST_PIPELINE (appdata.pipeline));
+		if (clock != NULL) {
+			GstClockTime cur;
+
+			cur = gst_clock_get_time(clock);
+			start_time = cur + (CLOCK_START_TIME * GST_SECOND);
+			id = gst_clock_new_periodic_id(clock, start_time, interval * GST_SECOND);
+			if (id == NULL) {
+				g_printerr("Error: failed to get clock id\n");
+				goto exit;
+			}
+			gst_clock_id_wait_async(id, (GstClockCallback) timeout_reached, &appdata, NULL);
+			gst_clock_id_wait(id, NULL);
+		}
+	}
+
+	/* Iterate */
+	g_print ("Running...\n");
+	g_main_loop_run (appdata.loop);
 
 
-    /* Out of the main loop, clean up nicely */
-    g_print ("Returned, stopping playback\n");
-    gst_element_set_state (appdata.pipeline, GST_STATE_NULL);
+	/* Out of the main loop, clean up nicely */
+	g_print ("Returned, stopping playback\n");
+	gst_element_set_state (appdata.pipeline, GST_STATE_NULL);
 
-    g_print ("Deleting pipeline\n");
-    gst_object_unref (GST_OBJECT (appdata.pipeline));
-    g_source_remove (bus_watch_id);
-    g_main_loop_unref (appdata.loop);
+exit:
+	/* unref clock resources */
+	if (id != NULL) {
+		gst_clock_id_unschedule (id);
+		gst_clock_id_unref(id);
+	}
+	if (clock != NULL)
+		gst_object_unref (clock);
 
-    return 0;
+	g_print ("Deleting pipeline\n");
+	gst_object_unref (GST_OBJECT (appdata.pipeline));
+	g_source_remove (bus_watch_id);
+	g_main_loop_unref (appdata.loop);
+
+	return 0;
 }
