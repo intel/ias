f9f5ab66b2f96fbe82d23a5f6a70ff3803d2aa2d
Pekka Paalanen <pekka.paalanen@collabora.com>
2019-03-29 17:07:34 +0200

    Rename plugin-registry.h to libweston/plugin-registry.h
    See "Rename compositor.h to libweston/libweston.h" for rationale.
    
    Signed-off-by: Pekka Paalanen <pekka.paalanen@collabora.com>

--- libweston/compositor.c
+++ libweston/compositor.c
@@ -69,7 +69,7 @@
 #include "shared/timespec-util.h"
 #include "git-version.h"
 #include "version.h"
-#include "plugin-registry.h"
+#include <libweston/plugin-registry.h>
 #include "pixel-formats.h"
 #include "trace-reporter.h"
 
